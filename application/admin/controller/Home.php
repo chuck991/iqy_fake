039b74e169d6779ac5f4f49673fa8690be7f6e23
chuck991 <chuck991@126.com>
2018-04-28 01:17:08 +0800

    后台页面展示预览

--- application/admin/controller/Home.php
+++ application/admin/controller/Home.php
@@ -4,12 +4,21 @@ namespace app\admin\controller;
 
 use Utils\data\Sysdb;
 
+/**
+ * 后台管理首页
+ * @package app\admin\controller*
+ */
 class Home extends BaseAdmin
 {
     //后台首页
     public function index()
     {
         $this->assign('admin',session('admin'));
+        return $this->fetch();
+    }
+    //后台主页欢迎界面
+    public function welcome()
+    {
         return $this->fetch();
     }
 }
